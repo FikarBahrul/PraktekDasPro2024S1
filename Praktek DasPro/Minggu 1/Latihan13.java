4c13d4276271fd9f3d136c510c90aa498696168e
FikarBahrul <Fikarbahrul@gmail.com>
2024-09-04 15:37:26 +0700

    hello

---
+++ Praktek DasPro/Minggu 1/Latihan13.java
@@ -0,0 +1,10 @@
+/** 
+ *Latihan13
+ */
+public class Latihan13 {
+
+    public static void main(String [] args) {
+        System.out.print("Nama Saya Fikar Bahrul Santoso");
+        //System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi -POLINEMA");
+    }
+}
