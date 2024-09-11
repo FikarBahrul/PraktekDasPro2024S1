9e3c224884c3d3b6f91574097db9aa9ec9b3e84f
FikarBahrul <Fikarbahrul@gmail.com>
2024-09-11 14:10:50 +0700

    Jobsheet 3 (Case Study)

---
+++ Minggu 3/ContohVariabel13.java
@@ -0,0 +1,15 @@
+public class ContohVariabel13 {
+
+    public static void main(String[] args) {
+        String salahSatuHobySayaAdalah = "Bermain Petak Umpet";
+        Boolean isPandai = true;
+        char jenisKelamin ='L';
+        byte _umurSayaSekarang = 20;
+        double $ipk = 3.24 , tinggi = 1.78;
+        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai ? " + isPandai);
+        System.out.println("jenisKelamin : " + jenisKelamin);
+        System.out.println("Umurku saat ini : " + _umurSayaSekarang);
+        System.out.println(String.format("saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
