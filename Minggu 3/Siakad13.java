20833c1545a4ce82ecc8ed4489d698928d0aa18a
FikarBahrul <Fikarbahrul@gmail.com>
2024-09-11 16:50:21 +0700

    Siakad

--- Minggu 3/Siakad13.java
+++ Minggu 3/Siakad13.java
@@ -6,7 +6,7 @@ public class Siakad13 {
         String  nama, nim;
         char kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
 
         System.out.print("Masukkan nama : ");
         nama = sc.nextLine();
@@ -21,10 +21,12 @@ public class Siakad13 {
         nilaiKuis = sc.nextDouble();
         System.out.print("Masukkan nilai tugas : ");
         nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai ujian : ");
-        nilaiUjian = sc.nextDouble();
+        System.out.print("Masukkan nilai UTS : ");
+        nilaiUTS = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS : ");
+        nilaiUAS = sc.nextDouble();
 
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian)/3;
+        nilaiAkhir = (nilaiKuis * 0.20 + nilaiTugas * 0.15 + nilaiUTS * 0.30 + nilaiUAS * 0.35);
 
         System.out.println("Nama : " + nama +" NIM : " + nim);
         System.out.println("Kelas : " + kelas +"Absen : " + absen);
