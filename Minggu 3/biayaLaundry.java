fe5ba55e8e93b9882ddd7656b8c0976e7f761fd3
FikarBahrul <Fikarbahrul@gmail.com>
2024-09-27 18:42:00 +0700

    Del Minggu 5

---
+++ Minggu 3/biayaLaundry.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class biayaLaundry {
+    public static void main(String[] args) {
+        Scanner cLaundry = new Scanner(System.in);
+        int biaya = 2000;
+        
+        System.out.print("Masukkan Nominal Bayar :");
+        double jmlBiaya = cLaundry.nextDouble();
+
+
+    }
+}
