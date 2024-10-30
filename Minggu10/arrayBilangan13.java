2a64b27cf5c481257a507b041e6c2d9362ed38dd
FikarBahrul <fikarbahrul99@gmail.com>
2024-10-30 14:25:27 +0700

    menambahkan Minggu 10

--- Minggu10/arrayBilangan13.java
+++ Minggu10/arrayBilangan13.java
@@ -2,15 +2,22 @@ package Minggu10;
 
 public class arrayBilangan13 {
     public static void main(String[] args) {
-        int [] bil = new int[4];
+    /*int [] bil = new int[4];
         bil [0] = 5;
         bil [1] = 13;
         bil [2] = -7;
-        bil [3] = 17;
+        bil [3] = 17;*/ 
+        //Deklarasi Array
+        int [] bil = {5, 13, -7, 17};
 
-        System.out.println(bil[0]);
+        /*System.out.println(bil[0]);
         System.out.println(bil[1]);
         System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        System.out.println(bil[3]);*/
+
+        //Ubah Statement
+        for (int i=0;i<=4;i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
